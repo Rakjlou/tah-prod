00e234cafb9eccd2b02cd9e40c59c3279e4b2e3f
Noé Sierra Velasquez <rakjlou@gmail.com>
2025-10-17 19:52:09 +0200

    login page

--- server.js
+++ server.js
@@ -13,6 +13,10 @@ app.get('/', (req, res) => {
     res.render('index');
 });
 
+app.get('/login', (req, res) => {
+    res.render('login');
+});
+
 app.listen(PORT, () => {
     console.log(`Server running on http://localhost:${PORT}`);
 });
